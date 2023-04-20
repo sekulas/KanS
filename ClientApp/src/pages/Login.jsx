a069cb94c87674d2f6d30ac8fe998b2ed547194d
sekulas <sekula.sebastian22@gmail.com>
2023-04-20 20:07:22 +0200

    F-E Login page

--- ClientApp/src/pages/Login.jsx
+++ ClientApp/src/pages/Login.jsx
@@ -1,6 +1,55 @@
+import { Box, TextField } from '@mui/material'
+import { useState } from 'react'
+import LoadingButton from '@mui/lab/LoadingButton'
+
 const Login = () => {
+
+    const [loading, setLoading] = useState(false)
+
+    const handleSubmit = () => {
+
+    }
+
     return (
-        <div>Login</div>
+        <>
+            <Box
+                component='form'
+                sx={{ mt:1 }}
+                onSubmit={handleSubmit}
+                noValidate
+            >
+                <TextField
+                    margin='normal'
+                    required
+                    fullWidth
+                    id='email'
+                    label='Email'
+                    name='email'
+                    type='email'
+                    disabled={loading}
+                />
+                <TextField
+                    margin='normal'
+                    required
+                    fullWidth
+                    id='password'
+                    label='Password'
+                    name='password'
+                    type='password'
+                    disabled={loading}
+                />
+                <LoadingButton
+                    sx={{mt:3, mb:2}}
+                    variant='outlined'
+                    fullWidth
+                    color='success'
+                    type='submit'
+                    loading={loading}
+                >
+                    Login
+                </LoadingButton>
+            </Box>
+        </>
     )
 }
 
