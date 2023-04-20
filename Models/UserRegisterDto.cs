2d2f24b1f1e975d6aab8bc929cd7698b12c1d8a2
sekulas <sekula.sebastian22@gmail.com>
2023-04-20 22:03:05 +0200

    B-E removed ConfirmPassword field from User

--- Models/UserRegisterDto.cs
+++ Models/UserRegisterDto.cs
@@ -4,5 +4,4 @@ public class UserRegisterDto {
     public string Name { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public string ConfirmPassword { get; set; }
 }
