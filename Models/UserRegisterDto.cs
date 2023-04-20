a878c303ee639ecb618466c0484b8361c3b870ee
sekulas <sekula.sebastian22@gmail.com>
2023-04-20 22:54:51 +0200

    B-E Checking confpassword and password in backend

--- Models/UserRegisterDto.cs
+++ Models/UserRegisterDto.cs
@@ -4,4 +4,5 @@ public class UserRegisterDto {
     public string Name { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
 }
