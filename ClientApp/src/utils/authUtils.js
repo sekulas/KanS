703ae26b76abf761276d49129ea84d00c9d24ec3
sekulas <sekula.sebastian22@gmail.com>
2023-05-02 12:51:50 +0200

    F-E changed isAuthenticated to return some user data

--- ClientApp/src/utils/authUtils.js
+++ ClientApp/src/utils/authUtils.js
@@ -1,7 +1,22 @@
+import jwt_decode from "jwt-decode";
+
+const baseURL = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/";
+
 const authUtils = {
   isAuthenticated: () => {
     const token = localStorage.getItem("token");
-    return !!token;
+
+    if (token) {
+      const decodedToken = jwt_decode(token);
+
+      const user = {
+        name: decodedToken[baseURL + "name"],
+        email: decodedToken[baseURL + "emailaddress"],
+      };
+      return user;
+    }
+
+    return false;
   },
 };
 
