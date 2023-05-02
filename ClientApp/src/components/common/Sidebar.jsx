dbf50dea853fdc24a87a6581c852161931c8e7dd
sekulas <sekula.sebastian22@gmail.com>
2023-05-02 20:42:50 +0200

    F-E sidebar fundamentals

--- ClientApp/src/components/common/Sidebar.jsx
+++ ClientApp/src/components/common/Sidebar.jsx
@@ -1,6 +1,48 @@
+import { Drawer, IconButton, Typography, List, ListItemButton, Box } from "@mui/material"
+import { useSelector, useDispatch } from "react-redux"
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined'
+import assets from '../../assets/index'
+
 const Sidebar = () => {
+    const user = useSelector( (state) => state.user.value )
+    const sidebarWidth = 250
+
     return (
-        <div>Sidebar</div>
+        <Drawer
+         container={window.document.body}
+         variant='permanent'
+         open={true}
+         sx={{
+            width: sidebarWidth,
+            height: '100%',
+            '& > div': { borderRight: 'none' }
+         }}
+        >
+            <List
+             disablePadding
+             sx={{
+                width: sidebarWidth,
+                height: '100vh',
+                backgroundColor: assets.colors.secondary
+             }}
+            >
+                <ListItemButton>
+                    <Box sx={{
+                        width: '100%',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between'
+                    }}>
+                        <Typography variant='body2' fontWeight='700'>
+                            {user.name}
+                        </Typography>
+                        <IconButton>
+                            <LogoutOutlinedIcon fontSize='small'/>
+                        </IconButton>
+                    </Box>
+                </ListItemButton>
+            </List>
+        </Drawer>
     )
 }
 
