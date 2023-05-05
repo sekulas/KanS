6b65c119cf20a6f63855a7d4cf0a851e39f42bb7
sekulas <sekula.sebastian22@gmail.com>
2023-05-05 18:26:26 +0200

    F-E Board page schema

--- ClientApp/src/api/boardApi.js
+++ ClientApp/src/api/boardApi.js
@@ -3,6 +3,7 @@ import axiosClient from "./axiosClient";
 const boardApi = {
   create: () => axiosClient.post("board"),
   getAllForUser: () => axiosClient.get("board"),
+  getOne: (boardId) => axiosClient.get(`board/${boardId}`),
 };
 
 export default boardApi;
--- ClientApp/src/pages/Board.jsx
+++ ClientApp/src/pages/Board.jsx
@@ -1,6 +1,103 @@
+import { useEffect, useState  } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box, IconButton, TextField, Typography, Button } from '@mui/material'
+import StarOutlinedIcon from '@mui/icons-material/StarOutlined'
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined'
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined'
+import boardApi from '../api/boardApi'
+
 const Board = () => {
+    const { boardId } = useParams()
+    const [name, setName] = useState('')
+    const [description, setDescription] = useState('')
+    const [sections, setSections] = useState([])
+    const [isFavourite, setIsFavourite] = useState(false)
+    const [icon, setIcon] = useState('')
+
+    useEffect(() => {
+
+        const getBoard = async () => {
+            try {
+                const res = await boardApi.getOne(boardId)
+                setName(res.name)
+                setDescription(res.description)
+                setSections(res.sections)
+                setIsFavourite(res.favourite)
+                setIcon(res.icon)
+            } catch(err) {
+                alert(err)
+            }
+        }
+
+        getBoard()
+    }, [boardId])
+
     return (
-        <div>Board</div>
+        <>
+            <Box sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                width: '100%'
+            }}>
+                <IconButton variant='outlined'>
+                {
+                    isFavourite ? (
+                        <StarOutlinedIcon color='warning'/>
+                    ) : (
+                        <StarBorderOutlinedIcon/>
+                    )
+                }
+                </IconButton>
+                <IconButton variant='outlined' color='error'>
+                    <DeleteOutlinedIcon/>
+                </IconButton>
+            </Box>
+            <Box sx={{
+                padding: '10px 50px'
+            }}>
+                <Box>
+                    {/*emocjipicker*/}
+                </Box>
+                <TextField
+                    value={name}
+                    placeholder='New Board #?'
+                    variant='outlined'
+                    fullWidth
+                    sx={{
+                        '& .MuiOutlinedInput-input': {padding: 0},
+                        '& .MuiOutlinedInput-notchedOutlined': {border: 'unset'},
+                        '& .MuiOutlinedInput-root': {fontSize: '2rem', fontWeight: '700'},
+                    }}
+                />
+                <TextField
+                    value={description}
+                    placeholder='Description goes here...'
+                    variant='outlined'
+                    multiline
+                    fullWidth
+                    sx={{
+                        '& .MuiOutlinedInput-input': {padding: 0},
+                        '& .MuiOutlinedInput-notchedOutlined': {border: 'unset'},
+                        '& .MuiOutlinedInput-root': {fontSize: '0.8rem'},
+                    }}
+                />
+            </Box>
+            <Box>
+                <Box sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between'
+                }}>
+                    <Button>
+                        Add section
+                    </Button>
+                    <Typography variant='body2' fontWeight='7'>
+                        {sections.length} Sections
+                    </Typography>
+                </Box>
+            </Box>
+        </>
     )
 }
 
